22a2d7aab88358556c611fe5a6095e400da277d0
rallapalliyaswanthkumar <yaswanthrallapalli08@gmail.com>
2026-06-24 22:21:49 +0530

    Resolved issues

--- fitness-service/fitness-service/src/main/java/com/wipro/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.wipro.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- user-service/user-service/src/main/java/com/wipro/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.entity;
-
-public enum Role {
-
-	USER,
-	ADMIN
-}
